6d76177750893d30db98be04f962ed2fdab258f9
sara-singh <ssingh8@tcd.ie>
2024-02-05 23:50:34 +0000

    Feat(17 - custom actions)Install missing packages and provide instructions for implementation

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,5 +1,21 @@
 const core =  require('@actions/core')
 async function run() {
+
+    /*
+    1. Parse inputs: 
+      1.1 Base branch from which to check for updates
+      1.2 target-branch to use to create the PR
+      1.3 Github Token for authentication purposes to create files
+      1.4 Working directory for which to check for dependencies
+
+    2. Execute the npm update command within the working directory
+    3. Check whether there are modified package*.json files
+    4. If there are modified files:
+        4.1 Add and commit files to the target branch
+        4.2 Create a pR to the base-branch using the octokit API
+    5. Otherwise, conclude the custom action
+    git 
+    */
     core.info('I am a custom JS action');
 }
 
