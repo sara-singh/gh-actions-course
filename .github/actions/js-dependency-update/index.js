d13f2a064bf9d16778a43434319ddf7e22f585f6
sara-singh <ssingh8@tcd.ie>
2024-02-06 01:18:31 +0000

    feat(17-custom-actions): parse and validate inputs1

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -8,7 +8,7 @@ async function run() {
     const targetBranch = core.getInput('target-branch');
     const ghToken = core.getInput('gh-token');
     const workingDir = core.getInput('working-directory');
-    const debug = core.getBooleanInput('Debug');
+    const debug = core.getBooleanInput('debug');
 
     core.setSecret(ghToken);
 
